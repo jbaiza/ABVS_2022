dcd234663c89eab71b9a191306498172058de029
Davids <davids.dubkevics@gmail.com>
2022-11-28 19:57:23 +0200

    6 uzdevums.

---
+++ uzdevums_6.py
@@ -0,0 +1,11 @@
+print("Ievadiet skaitli ar kuru velaties parbaudit vai sakitlis dalas ar 5 vai 7 :")
+n=input()
+if int(n)%7==0:
+  print("Ar 7 dalas")
+else:
+  print("Ar 7 nedalas")
+
+if int(n)%5==0:
+  print("Ar 5 dalas")
+else:
+  print("Ar 5 nedalas")
